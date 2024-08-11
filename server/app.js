7377836d0670be62c0434241c7256a46c5dc277c
shivanand_khudanapur <155430597+shiv-sk@users.noreply.github.com>
2024-08-11 21:37:29 +0530

    Update app.js
    origin issue

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const corsoption = {
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173",
     https:"https://blog-app-r95h.vercel.app"
-    
+    ]
 }
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
